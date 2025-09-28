99de9e8db038857a75b0d802693194884381e77f
Eissa Purnell <eissamonet@gmail.com>
2025-09-28 18:06:22 -0500

    commit changes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const menuOpenButton = document.querySelector('#menu-open-button');
 
+const menuCloseButton = document.querySelector('#menu-close-button');
+
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
+
+// close the mobile menu when clicking the close button
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
